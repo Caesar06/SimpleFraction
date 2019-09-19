6652978432c95a1c454687a412b8382f615213fb
Caesar06 <caesarait067@gmail.com>
2019-09-19 11:28:03 +0200

    first commit

---
+++ SimpleFraction/src/Fraction.java
@@ -0,0 +1,18 @@
+
+public class Fraction {
+
+	private int num;
+	private int denum;
+	
+	public Fraction(int num, int denum)
+	{
+		this.num = num;
+		this.denum = denum;
+	}
+	
+	public String toString()
+	{
+		return num + "/" + denum;
+	}
+	
+}
---
+++ SimpleFraction/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	
+	public static void main(String [ ] args)
+	{
+		Fraction F = new Fraction(30,15);
+		System.out.println(F.toString());
+	}
+	
+}
